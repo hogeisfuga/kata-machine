e1984f218d7451c05c989c4f5cca493568da20dc
Aki <120ce066@gmail.com>
2024-03-16 17:14:19 +0900

    キューの実装

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,58 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>
+  private tail?: Node<T>
 
+  constructor() {
+    this.head = this.tail = undefined
+    this.length = 0
+  }
+
+  /**
+   * enque item
+   * @param item 
+   * @returns 
+   */
+  enqueue(item: T): void {
+    const node: Node<T> = { value: item }
     
-
-    constructor() {
+    this.length++
+    
+    if (this.length === 1) {
+      this.tail = this.head = node
+      return
     }
+    // tailだったnodeのnextにtailとなるnodeへのリンクを作る
+    this.tail!.next = node
+    this.tail = node
 
-    enqueue(item: T): void {
+  }
+  /**
+   * deque item. if nothing enqued, return undefined
+   * @returns T | undefined
+   */
+  deque(): T | undefined {
+    if (!this.head) return undefined
+    
+    this.length--
+    
+    const head = this.head
+    this.head = head.next
+    head.next = undefined
+    
+    if (this.length === 0) {
+      this.tail = undefined
+    }
+    
+    return head.value
+  }
 
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+  peek(): T | undefined {
+    return this.head?.value
+  }
 }
